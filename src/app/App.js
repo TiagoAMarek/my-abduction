6bafb2dbc87373c3eedc6f6cbb64c3d347b8f199
Tiago Marek <tiagomarek@gmail.com>
2018-06-15 23:32:13 -0300

    Page style
    - [x] add some images that will be used in the near future.
    - [x] add normalize.css to reset any default default css rules.
    - [x] sets `box-sizing` to `border-box` in `*`.
    - [x] moving futura's `font-face` to index.css`.
    - [x] sets body's background color to `#222`.
    - [x] sets html and body's height size to `100%`.
    - [x] creates page title basic html/css in App component files.
    - [x] page basic layout.

--- src/app/App.js
+++ src/app/App.js
@@ -5,8 +5,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <AppBar />
+
+        <main>
+          <div className="page__title--wrapper">
+            <h2>Resistance</h2>
+          </div>
+          <div className="page__content">
+            <div className="card">card</div>
+            <div className="card">card</div>
+            <div className="card">card</div>
+          </div>
+        </main>
       </div>
     );
   }
