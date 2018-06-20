9ded31b45a68dbcd29e95e7f794b3c89dcda7219
Tiago Alves Marek <tiagomarek@tiagomarek.local>
2018-06-20 16:51:34 -0300

    Create page component
    - [x] Turning page into an component

--- src/app/App.js
+++ src/app/App.js
@@ -7,17 +7,6 @@ class App extends Component {
     return (
       <div className="app">
         <AppBar />
-
-        <main>
-          <div className="page__title--wrapper">
-            <h2>Resistance</h2>
-          </div>
-          <div className="page__content">
-            <div className="card">card</div>
-            <div className="card">card</div>
-            <div className="card">card</div>
-          </div>
-        </main>
       </div>
     );
   }
